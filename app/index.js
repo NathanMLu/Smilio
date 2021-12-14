e8458bb80c547f28476f56631398bf9bf507e99f
Nathan Lu <nathanlumax@gmail.com>
2021-12-14 02:46:31 -0600

    Very rough, but we have a working game, with self generated levels and increments to timer

--- app/index.js
+++ app/index.js
@@ -1,12 +1,16 @@
 import document from "document";
 import { display } from "display";
+import { vibration } from "haptics";
 
 const STARTINGTIME = 15;
+const NUMFACES = 9;
+const GAPBETWEEN = 70;
+const faces = [];
 
-let smiley, cool, frown1, frown2, neutral, sad, smile1, smile2, smile3;
 let innertimer, outertimer;
 let running = false;
 let timer = STARTINGTIME;
+
 
 display.addEventListener("change", () => {
     if (display.on) {
@@ -16,42 +20,19 @@ display.addEventListener("change", () => {
     }
 });
 
-function Start(){
-    smiley = document.getElementById("smiley");
-    cool = document.getElementById("cool");
-    frown1 = document.getElementById("frown1");
-    frown2 = document.getElementById("frown2");
-    neutral = document.getElementById("neutral");
-    sad = document.getElementById("sad");
-    smile1 = document.getElementById("smile1");
-    smile2 = document.getElementById("smile2");
-    smile3 = document.getElementById("smile3");
-
-    innertimer = document.getElementById("innertimer");
-    outertimer = document.getElementById("outertimer");
-
-    //smiley.style.display = "none";
-    cool.style.display = "none";
-    frown1.style.display = "none";
-    frown2.style.display = "none";
-    neutral.style.display = "none";
-    sad.style.display = "none";
-    smile1.style.display = "none";
-    smile2.style.display = "none";
-    smile3.style.display = "none";
-
-    timerHandler();
-    running = true;
+function getRandomInt(min, max){
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 function timerHandler(){
     const countdown = setInterval(function () {
         if (running) {
             timer -= 0.1;
-            innertimer.x -= (300/STARTINGTIME) * 0.1;
+            innertimer.width -= (300/STARTINGTIME) * 0.1;
             if (timer <= 0){
                 // Stops the timer
                 clearInterval(countdown);
+                vibration.start("ping");
 
                 console.log("Timer ended!");
                 running = false;
@@ -60,7 +41,87 @@ function timerHandler(){
     }, 100);
 }
 
-Start();
+function addTime(){
+
+    if(innertimer.width < 300){
+        timer += 2;
+        innertimer.width += (300/STARTINGTIME) * 2;
+    }
+}
+
+function clickHandler(){
+    for (let i = 0; i<NUMFACES; i++) (function(i) {
+        faces[i].onclick = function (event) {
+            if (running) {
+                if(faces[i].id == "smiley"){
+                    vibration.start("nudge");
+                    addTime();
+                    newLevel();
+                } else {
+                    vibration.start("bump");
+                }
+            }
+        }
+    })(i);
+}
+
+function newLevel(){
+    const x = [];
+    const y = [];
+    for (let i = 0; i<NUMFACES; i++){
+        const generated_x = getRandomInt(0,255);
+        const generated_y = getRandomInt(0,255);
+
+        let success = true;
+
+        // Loops through all previous positions to avoid overlaps
+        for (let j = 0; j < x.length; j++){
+            if (Math.abs(x[j] - generated_x) <= GAPBETWEEN && Math.abs(y[j] - generated_y) <= GAPBETWEEN) {
+                success = false;
+            }
+        }
+
+        if (success) {
+            faces[i].style.display = "inline";
+
+            faces[i].x = generated_x;
+            faces[i].y = generated_y;
+
+            x.push(generated_x);
+            y.push(generated_y);
+        }
+    }
+}
+
+function refresh(){
+    for (let i = 0; i<NUMFACES; i++){
+        faces[i].style.display = "none";
+    }
+}
+
+function start(){
+    faces[0] = document.getElementById("smiley");
+    faces[1] = document.getElementById("cool");
+    faces[2] = document.getElementById("frown1");
+    faces[3] = document.getElementById("frown2");
+    faces[4] = document.getElementById("neutral");
+    faces[5] = document.getElementById("sad");
+    faces[6] = document.getElementById("smile1");
+    faces[7] = document.getElementById("smile2");
+    faces[8] = document.getElementById("smile3");
+
+    innertimer = document.getElementById("innertimer");
+    outertimer = document.getElementById("outertimer");
+
+    refresh();
+    newLevel();
+    timerHandler();
+    clickHandler();
+
+    running = true;
+}
+
+start();
 
 
 
